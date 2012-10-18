cf1923b478db0b88a4f6d72015d53a1cb74253da
Guasco <noemailshere@paradeincomodar.com>
2012-10-18 19:58:24 -0300

    teste de struck e maloc na funcao pedro

--- teste1.cpp
+++ teste1.cpp
@@ -1,8 +1,21 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct NODO {
+  int var1;
+  float var2;
+  struct NODO *next;
+} NODO;
+
+
 void pedro(){
  printf("fuckyeahhh\n");
+ NODO *minhaThread;
+ minhaThread=(NODO *) malloc(sizeof(NODO) );
+ minhaThread->var1=4;
+ printf("fuckyeahhh %i\n", minhaThread->var1);
+ //minhaThread = (*minhaThread)malloc(50);
 }
 
 void mike(){
