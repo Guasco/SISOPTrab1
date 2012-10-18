25c2585d7530a7d54e98e46b75f48ed0e8600f1c
Mike Muya <muyatshib@hotmail.com>
2012-10-18 19:15:10 -0300

    upload3

--- teste1.cpp
+++ teste1.cpp
@@ -1,6 +1,16 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+
+
+
+void mike(){
+
+printf("conseguiiiiiiiiii!!!\n");
+}
 int main(){
 	printf("a \noasijsoisa\nbc \n");
+	mike();
+	//system("pause");
 	return 0;
+
 }
