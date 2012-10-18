82909138cfadf5e0a667351c60617f0352dc1d33
Guasco <noemailshere@paradeincomodar.com>
2012-10-18 19:25:25 -0300

    alteracao de duas funces ao mesmo tempo

--- teste1.cpp
+++ teste1.cpp
@@ -1,7 +1,9 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-
+void pedro(){
+ printf("fuckyeahhh\n");
+}
 
 void mike(){
 
@@ -10,6 +12,7 @@ printf("eu quero terminar o trabalho de SISOP KCT!\n");
 int main(){
 	printf("a \noasijsoisa\nbc \n");
 	mike();
+	pedro();
 	//system("pause");
 	return 0;
 
