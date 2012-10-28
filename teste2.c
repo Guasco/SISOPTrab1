ad4dfc5fcfc29a4dffd920494d31a02a3e7d949e
Pedro <naoehdasuacontadotcom@com.com>
2012-10-28 18:06:59 -0300

    criacao da biblioteca

---
+++ teste2.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "uthread.h"
+
+Process *lista[15];
+
+int func1(int arg)
+{
+	
+    printf("func1: started\n");
+	
+	
+	
+	int x;
+	for(x=0;x<10;x++){
+	    printf("func1: yeld! ill be back!\n");
+		uthread_yield();
+		printf("func1: im back!!\n");
+	}
+    
+    printf("func1: cya\n");
+	return 0;
+
+}
+
+
+
+static void
+func2(int meuid)
+{
+	int x=0;
+	
+	if(meuid==6){
+		printf("Thread%d i need wait Thread8 finish, then i back\n", meuid);
+		join_thread(lista[8]);
+		printf("Thread%d the Thread8 has finished, here i go again\n", meuid);
+	}
+	
+	printf("Thread%d started\n", meuid);
+	 for(x=2;x<meuid;x++){
+		printf("Thread%d computation %d\n",meuid, x);
+		printf("Thread%d yeld! ill be back!\n", meuid);
+		uthread_yield();
+		printf("Thread%d im back!!\n", meuid);
+	}		
+	printf("Thread%d near finishing, but not now.\n", meuid);
+	uthread_yield();
+	
+    printf("Thread%d cya\n", meuid);
+}
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+	
+	
+	int x;
+	for(x=0;x<15;x++){
+		lista[x]=uthread_create(func2,(void *)x);
+	}
+
+	//Lista fake prototipo
+	lista[0]->next=lista[1];
+	lista[0]->prev=lista[14];
+	lista[14]->next=lista[0];
+	lista[14]->prev=lista[13];
+
+	for(x=1;x<14;x++){
+		lista[x]->next=lista[x+1];
+		lista[x]->prev=lista[x-1];
+	}
+	
+	// Process_debug(Running_thread);	
+	join_thread(lista[7]);
+	Process_debug(lista[7]);
+	printf("main: le escalonateur, its me, ill join again\n");
+	join_thread(lista[10]);
+	
+	printf("main: le escalonateur, its me again , ill join again\n");
+	
+	join_thread(lista[14]);
+	
+	
+	
+	// myProc=create_thread(func1,(void *)i);
+	// myProc2=create_thread(func2,(void *)i);
+	
+	// start_thread(myProc);
+	
+	// while(myProc->sleeping){
+		// if(myProc->sleeping)
+			// start_thread(myProc);
+
+		// if(myProc2->sleeping)
+			// start_thread(myProc2);
+	// }
+	
+
+    
+    printf("main: exiting\n");
+	fila_tests();
+    exit(EXIT_SUCCESS);
+}
---
+++ uthread.h
@@ -0,0 +1,73 @@
+#include <ucontext.h>
+
+#define STACKSIZE 32768
+#define TAMANHO_NOME 30
+#define FILA_VAZIA -1
+
+enum{
+Running,
+Blocked,
+Ready,
+Finished
+
+};
+static char *enumDesc[] = {"Running", "Blocked", "Ready","Finished" };
+typedef struct Process {
+  int var1;
+  int id;
+  float var2;
+  ucontext_t contexto;
+  ucontext_t caller;
+  // int sleeping;
+  int state;
+  char stack[STACKSIZE];
+
+  struct Process *next;
+  struct Process *prev;
+
+} Process;
+
+struct elemento {char *nome;
+                 int valor;
+                 struct elemento *proximo;
+                 struct elemento *anterior;
+                 };
+
+struct fila {
+    struct elemento *primeiro;
+    struct elemento  *ultimo;
+    int contador;
+    };
+
+void Process_debug(Process *myProc);
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+
+typedef struct fila Fila;
+
+//funcao para armazenar a estutura base na fila
+struct fila *criaFila();
+//* Função para armazenar um elemento da fila */
+
+struct elemento *criaElemento();
+/* Função que insere um elemento na fila */
+
+int insereElemento (struct elemento* ptrElemento, struct fila* ptrFila, int posicao);
+
+void Debug_elemento(struct elemento *myElemento);
+
+void fila_tests();
+	
+void uthread_yield();
+
+Process* uthread_create( void* func , void *arg);
+
+int run_thread(Process *myProc);
+void join_thread(Process *thread_that_must_finish);
+
+
+
+
+
