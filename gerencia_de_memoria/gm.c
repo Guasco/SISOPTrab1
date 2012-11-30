5b03d112939b7b66377cdcba5e771b41fcbc3188
Pedro <naoehdasuacontadotcom@com.com>
2012-11-29 22:27:26 -0300

    algoritmos funcionando
    Falta Tranformar em lib, testar, remover msgs, revisar especificacao.

--- gerencia_de_memoria/gm.c
+++ gerencia_de_memoria/gm.c
@@ -2,52 +2,170 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define STACKSIZE 32768
+#define LRU_MODE 1
+#define FIFO_MODE 2
 
 
 
-int *frames;
-int *pages;
+
+int *page_on_frame;
+int *frame_assigned_to_page;
+int *frame_time;
 int MAX_PAGE=0;
 int MAX_FRAME=0;
+int FRAME_VITIMA=0;
+int mem_time=0;
+
+///SELECAO DE MODO//
+int MODE=LRU_MODE;
+// int MODE=FIFO_MODE;
 
 int mv_start(int num_pages, int num_frames){
 
-	frames= malloc(sizeof(int)*num_frames );
-	pages= malloc(sizeof(int)*num_pages );
+	page_on_frame= malloc(sizeof(int)*num_frames );
+	frame_assigned_to_page= malloc(sizeof(int)*num_pages );
+	frame_time= malloc(sizeof(int)*num_pages );
 	MAX_PAGE=num_pages;
 	MAX_FRAME =num_frames;
 	
 	int x;
-	for(x=0;x<num_pages;x++){	pages[x]=0;	}
-	for(x=0;x<num_frames;x++){	frames[x]=0;}
+	for(x=0;x<num_pages;x++){	
+		frame_assigned_to_page[x]=-1;	
+	}
+	for(x=0;x<num_frames;x++){	
+		frame_time[x]=0;	
+		page_on_frame[x]=-1;
+	}
 	
 	// frames= malloc(sizeof(int) );
 	// pages= malloc(sizeof(int) );
 	
 }
+
+
 int mv(int page, int *frame){
-	*frame=page;
+	switch(MODE){
+		case FIFO_MODE: return mv_fifo(page,frame);
+		break;
+		case LRU_MODE: return mv_lru(page,frame);
+		break;
+	}
+
 }
+#define page_miss frame_assigned_to_page[this_page]==-1
+#define page_vitima page_on_frame[FRAME_VITIMA]
+#define SWAP_OUT -1
+#define FREE -1
+#define NONE -1
+#define this_frame frame_assigned_to_page[this_page]
+#define print(a)  printf("%i\n",a)
+
+
+int mv_lru(int this_page, int *frame){
+	// pages[page]=1;
+	int result=0;
+	mem_time++;
+
+	if(page_miss){
+		printf("page miss %i => %i  (%i)  ",this_page,FRAME_VITIMA, page_vitima);
+		frame_assigned_to_page[page_vitima]=SWAP_OUT;
+		this_frame=FRAME_VITIMA;
+		page_on_frame[FRAME_VITIMA]=this_page;
+		frame_time[this_frame]=mem_time;
+		result =1;
+		int x=0;
+		int know_oldest=frame_time[FRAME_VITIMA]+1;
+		int min=mem_time;
+		int found=0;
+		
+		
+		while(x<MAX_FRAME && !found	){
+			if(page_on_frame[x]==NONE
+			|| frame_time[x]==know_oldest)
+			{
+				// print("page_on_frame[%i]=%i\n",x,page_on_frame[x]);
+				// print("frame_time[x]=%i\n",x,frame_time[x]);
+				// print("frame_time[x]=%i\n",x,frame_time[x]);
+				FRAME_VITIMA=x;
+				// printf("\nx:%i\n",x);
+				found=1;
+			}else if(min>frame_time[x]){
+				min=frame_time[x];
+				FRAME_VITIMA=x;
+			}
+			x++;
+		}
+	}/*elseif(HIT)*/else{
+		printf("hit! page(%i) => frame(%i)    ",this_page,this_frame);
+		frame_time[this_frame]=mem_time;
+		
+	}
+	
+	printf("next_vitima(%i)\n",FRAME_VITIMA);
+	*frame=this_frame;
+	
+	int x;
+	for(x=0;x<MAX_FRAME;x++){
+		printf("Frame[%i] Page= %i   FrameTime=%i\n",x,page_on_frame[x],frame_time[x] );
+	
+	}
+	printf("\n");
+	return result;
+}
+
+int mv_fifo(int this_page, int *frame){
+	// pages[page]=1;
+	int result=0;
+	
+	if(page_miss){
+		printf("page miss page(%i) => frame(%i)  page_out(%i)  \n",this_page,FRAME_VITIMA, page_vitima);
+		frame_assigned_to_page[page_vitima]=SWAP_OUT;
+		this_frame=FRAME_VITIMA;
+		page_on_frame[FRAME_VITIMA]=this_page;
+		result =1;
+		FRAME_VITIMA++;
+	}else{
+		printf("hit! page(%i) => frame(%i)    \n",this_page,this_frame);
+	}
+	
+	*frame=this_frame;
+	if(FRAME_VITIMA==MAX_FRAME){
+		FRAME_VITIMA=0;
+	}
+	return result;
+}
+
 
 int main(int argc, char *argv[])
 {
-
-	mv_start(5000, 1000);
-	frames[0]=0;
-	frames[1]=1;
-	frames[2]=2;
-	int *meuframe;
+	
+	mv_start(5, 3);
+	
+	
+	// mv(5,meuframe);
 	int x;
 	// for(x=0;x<5000;x++){
 		// frames[x]=x*1000+1;
 	// }
+	int *meuframe;
 	meuframe = (int*) malloc(sizeof(int));
 	*meuframe=7;
 	int minha_pagina=16;
-	mv(minha_pagina, meuframe);
-	for(x=0;x<5000;x++){
-		printf("abc  %i \n",frames[x]);
-	}
+	mv(1, meuframe);
+	mv(1, meuframe);
+	mv(1, meuframe);
+	mv(2, meuframe);
+	mv(3, meuframe);
+	mv(4, meuframe);
+	mv(0, meuframe);
+	mv(2, meuframe);
+	mv(2, meuframe);
+	printf("abc  %i \n",*meuframe);
+	print(page_on_frame[2]);
+	
+	// for(x=0;x<5000;x++){
+		// printf("abc  %i \n",frames[x]);
+	// }
 }
 
 
